18bf806113ae65d98babe30107276e0818638c89
Ceyda Ulubas <ceyda.ulubas@gmail.com>
2021-05-08 00:41:07 +0300

    added some items for movie detail page, but should be continue

--- src/components/Pages/Home/MovieDetail.js
+++ src/components/Pages/Home/MovieDetail.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { Container, Row, Col } from 'react-bootstrap'
+
+import '../../../style/MovieDetail.scss';
 
 const MovieDetail = (props) => {
 
@@ -8,28 +11,57 @@ const MovieDetail = (props) => {
     console.log('details', details);
     console.log("props.match.params", props.match.params);
 
+
     const getSingleMovie = () => {
 
         console.log(props.match.params);
         // get the 'id' from url via 'props.match.params' object
-        const {id} = props.match.params;
+        const { id } = props.match.params;
         // api call to the server to retrieve a single object
-        axios
-        
-          .get(`https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`)
-          .then((responseFromApi) => {
-            setDetails(responseFromApi.data);
-          })
-          .catch((error) => console.error(error));
-      };
 
-      useEffect(getSingleMovie, [props.match.params]);
+        axios
+            .get(`https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`)
+            .then((responseFromApi) => {
+                setDetails(responseFromApi.data);
+            })
+            .catch((error) => console.error(error));
+    };
+
+    useEffect(getSingleMovie, [props.match.params]);
 
     return (
-        <div>
-      <p>{details.title}</p>
 
-    </div>
+        <div className="detail"
+            style={{
+                backgroundImage: `url(${`https://www.themoviedb.org/t/p/w220_and_h330_face/${details.poster_path}`}) `,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center center',
+                backgroundRepeat: 'no-repeat',
+            }}>
+
+            <Container>
+                <Row>
+                    <Col sm={4}>
+                        <img src={`https://www.themoviedb.org/t/p/w220_and_h330_face/${details.backdrop_path}`} alt="popularMovieImage" />
+                    </Col>
+                    <Col sm={8}>
+                    <div>
+                        <p>{details.title}</p>
+                        <section>{details.release_date} </section>
+                        <p> {details.name}</p>
+
+                        <p style={{ textTransform: 'capitalize' }}>{details.overview}</p>
+                        <h2>Overview</h2>
+                        <p> {details.overview} </p>
+                    </div>
+                        
+                    </Col>
+                </Row>
+            </Container>
+
+
+        </div>
+
 
     )
 }
