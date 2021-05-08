9a725fe397ec11c7b286c33162127b9a1a81ed27
Ceyda Ulubas <ceyda.ulubas@gmail.com>
2021-05-08 23:04:01 +0300

    detail page background image is done

--- src/components/Pages/Home/MovieDetail.js
+++ src/components/Pages/Home/MovieDetail.js
@@ -3,6 +3,12 @@ import axios from "axios";
 import { Container, Row, Col } from 'react-bootstrap'
 
 import '../../../style/MovieDetail.scss';
+import { IconButton } from '@material-ui/core';
+import ListIcon from '@material-ui/icons/List';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import BookmarkIcon from '@material-ui/icons/Bookmark';
+import StarIcon from '@material-ui/icons/Star';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
 
 const MovieDetail = (props) => {
 
@@ -33,31 +39,50 @@ const MovieDetail = (props) => {
 
         <div className="detail"
             style={{
-                backgroundImage: `url(${`https://www.themoviedb.org/t/p/w220_and_h330_face/${details.poster_path}`}) `,
-                backgroundSize: 'cover',
-                backgroundPosition: 'center center',
+                backgroundImage: `url(${`https://www.themoviedb.org/t/p/w1280/${details.backdrop_path}`}) `,
+                backgroundPosition: 'right -20% top',
                 backgroundRepeat: 'no-repeat',
-            }}>
+            }}
+        >
+            <div className="transparency">
 
-            <Container>
-                <Row>
-                    <Col sm={4}>
-                        <img src={`https://www.themoviedb.org/t/p/w220_and_h330_face/${details.backdrop_path}`} alt="popularMovieImage" />
-                    </Col>
-                    <Col sm={8}>
-                    <div>
-                        <p>{details.title}</p>
-                        <section>{details.release_date} </section>
-                        <p> {details.name}</p>
+                <Container>
+                    <Row>
+                        <Col sm={4} className="leftPart">
+                            <img src={`https://www.themoviedb.org/t/p/w220_and_h330_face/${details.poster_path}`} alt="popularMovieImage" />
+                        </Col>
+                        <Col sm={8}>
+                            <div className="rightPart">
+                                <h1>{details.title}</h1>
 
-                        <p style={{ textTransform: 'capitalize' }}>{details.overview}</p>
-                        <h2>Overview</h2>
-                        <p> {details.overview} </p>
-                    </div>
-                        
-                    </Col>
-                </Row>
-            </Container>
+                                   <p>{details.release_date} • {details.runtime}m</p>
+  
+                                
+                                <div className="circle">
+                                    <p className="ratioNum">{details.vote_average}</p>
+                                    <p className="voteAverage">Vote Average</p>
+                                    <div>
+                                        <IconButton className="iconCircle"> <ListIcon style={{ color: 'white' }} /></IconButton>
+                                        <IconButton className="iconCircle2"> <FavoriteIcon style={{ color: 'white', fontSize: 'small' }} /></IconButton>
+                                        <IconButton className="iconCircle3"> <BookmarkIcon className=" bookMark" style={{ color: 'white', fontSize: 'small' }} /></IconButton>
+                                        <IconButton className="iconCircle4"> <StarIcon style={{ color: 'white', fontSize: 'small' }} /></IconButton>
+                                        <IconButton className="iconCircle5"> <PlayArrowIcon style={{ color: 'white' }} /> <p className="playTitle">Play Trailer</p> </IconButton>
+
+
+                                    </div>
+                                </div>
+
+                                <p> {details.name}</p>
+
+                                <p style={{ textTransform: 'capitalize' }}><i>{details.tagline}</i></p>
+                                <h4>Overview</h4>
+                                <p> {details.overview} </p>
+                            </div>
+
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
 
 
         </div>
